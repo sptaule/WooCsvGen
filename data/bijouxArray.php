89ca4123fd06ea4d77424f545fd16dcf1d00b3d1
sptaule <lecas83@gmail.com>
2020-08-10 19:29:19 +0200

    Data for Bijoux theme
    Data for Bijoux theme

---
+++ data/bijouxArray.php
@@ -0,0 +1,99 @@
+<?php
+
+$listNames = array(
+    array(
+        "Name" => "Chevalière",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Chevalière",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Argent"
+    ),
+    array(
+        "Name" => "Chevalière",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Or"
+    ),
+    array(
+        "Name" => "Bague en rubis",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Bague en ambre",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Argent"
+    ),
+    array(
+        "Name" => "Bague en métal",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Or"
+    ),
+    array(
+        "Name" => "Bracelet",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Collier",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Chaîne",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Pendentif",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Boucles d'oreilles",
+        "Category" => "Bijoux homme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Diadème",
+        "Category" => "Bijoux femme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Serre-tête",
+        "Category" => "Bijoux femme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Collier ras du cou",
+        "Category" => "Bijoux femme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Montre",
+        "Category" => "Accessoires",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Piercing",
+        "Category" => "Accessoires",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Chaîne de cheville",
+        "Category" => "Bijoux femme",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Épingle à cheveux",
+        "Category" => "Accessoires",
+        "Subcategory" => "Acier"
+    ),
+    array(
+        "Name" => "Bouton de manchette",
+        "Category" => "Accessoires",
+        "Subcategory" => "Acier"
+    )
+);
