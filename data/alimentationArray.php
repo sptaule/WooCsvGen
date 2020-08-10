3a37c7fa72f58e1d238bb2a4d86fed4606c930ac
sptaule <lecas83@gmail.com>
2020-08-10 15:10:54 +0200

    Data for Alimentation theme
    Contains Name, Category, Subcategory

---
+++ data/alimentationArray.php
@@ -0,0 +1,63 @@
+<?php
+
+$listNames = array(
+    array(
+        "Name" => "Sirop de grenadine",
+        "Category" => "Sirops"
+    ),
+    array(
+        "Name" => "Sirop de menthe",
+        "Category" => "Sirops"
+    ),
+    array(
+        "Name" => "Sirop de myrtille",
+        "Category" => "Sirops"
+    ),
+    array(
+        "Name" => "Soda artisanal aux agrumes",
+        "Category" => "Boissons sucrées"
+    ),
+    array(
+        "Name" => "Soda à la fraise allégé en sucre",
+        "Category" => "Boissons sucrées"
+    ),
+    array(
+        "Name" => "Vin rouge",
+        "Category" => "Alcools"
+    ),
+    array(
+        "Name" => "Vin blanc",
+        "Category" => "Alcools"
+    ),
+    array(
+        "Name" => "Vin rosé",
+        "Category" => "Alcools"
+    ),
+    array(
+        "Name" => "Champagne",
+        "Category" => "Alcools"
+    ),
+    array(
+        "Name" => "Cidre",
+        "Category" => "Alcools"
+    ),
+    array(
+        "Name" => "Bière",
+        "Category" => "Alcools"
+    ),
+    array(
+        "Name" => "Liqueur de mirabelles",
+        "Category" => "Alcools"
+    ),
+    array(
+        "Name" => "Eau de vie",
+        "Category" => "Alcools"
+    ),
+    array(
+        "Name" => "Hydromel au cassis",
+        "Category" => "Alcools"),
+    array(
+        "Name" => "Thé à la canneberge",
+        "Category" => "Thés"
+    )
+);
