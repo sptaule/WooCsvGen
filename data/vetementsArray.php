bf85c9111b257fda8d4a7c4d58bab43fcd8ce25d
sptaule <lecas83@gmail.com>
2020-08-10 19:28:11 +0200

    Data for Vêtements theme
    Data for Vêtements theme

---
+++ data/vetementsArray.php
@@ -0,0 +1,64 @@
+<?php
+
+$listNames = array(
+    array(
+        "Name" => "Chaussettes d'été",
+        "Category" => "Chaussettes"
+    ),
+    array(
+        "Name" => "Chaussettes d'hiver",
+        "Category" => "Chaussettes"
+    ),
+    array(
+        "Name" => "Combinaison en coton",
+        "Category" => "Combinaisons"
+    ),
+    array(
+        "Name" => "T-shirt col en V",
+        "Category" => "T-shirts"
+    ),
+    array(
+        "Name" => "T-Shirt manches longues",
+        "Category" => "T-shirts"
+    ),
+    array(
+        "Name" => "Débardeur en polyester",
+        "Category" => "T-shirts"
+    ),
+    array(
+        "Name" => "Robe de chambre",
+        "Category" => "Confort"
+    ),
+    array(
+        "Name" => "Chaussures de ville",
+        "Category" => "Chaussures"
+    ),
+    array(
+        "Name" => "Bottines en jute",
+        "Category" => "Chaussures"
+    ),
+    array(
+        "Name" => "Caleçon en lin",
+        "Category" => "Sous-vêtements"
+    ),
+    array(
+        "Name" => "Culotte en sisal",
+        "Category" => "Sous-vêtements"
+    ),
+    array(
+        "Name" => "Pantalon en chanvre",
+        "Category" => "Pantalons"
+    ),
+    array(
+        "Name" => "Jupe en velour côtelé",
+        "Category" => "Jupes"
+    ),
+    array(
+        "Name" => "Chemise en toile",
+        "Category" => "Chemises"
+    ),
+    array(
+        "Name" => "Bonnet d'hiver",
+        "Category" => "Bonnets"
+    )
+);
