7159fe8b6fde071fdb20386e1d6705e3b041e519
sptaule <lecas83@gmail.com>
2020-08-13 20:30:03 +0200

    Added Saleprices option

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if(isset($_POST["generate"])){
 	$attributes = htmlspecialchars($_POST["attributs"]);
 	$subattributes = htmlspecialchars($_POST["sousattributs"]);
 	$qty = htmlspecialchars($_POST["productqty"]);
-	var_dump(globalGenerate($theme, $type, $visibility, $stock, $dimensions, $comments, $minprice, $maxprice, $qty));
+    var_dump(globalGenerate($theme, $type, $visibility, $stock, $dimensions, $comments, $minprice, $maxprice, $qty));
 }
 
 ?>
@@ -165,14 +165,23 @@ if(isset($_POST["generate"])){
 			  </div>
 
 			  <div class="Prix">
-			  	<section>
-					<h2>Prix minimum</h2>
-					<input id="range-control" type="range" min="5" max="50" step="5" value="5" name="minprice">
-					<span class="pricevalue minpricevalue">5 €</span>
-					<h2>Prix maximum</h2>
-					<input id="range-control-2" type="range" min="100" max="1000" step="10" value="100" name="maxprice">
-					<span class="pricevalue maxpricevalue">100 €</span>
-				</section>
+			  	<section style="display: flex; align-items: center;">
+                    <div style="width: 55%">
+                        <h2>Prix minimum</h2>
+                            <input id="range-control" type="range" min="5" max="50" step="5" value="5" name="minprice">
+                            <span class="pricevalue minpricevalue">5 €</span>
+                        <h2>Prix maximum</h2>
+                            <input id="range-control-2" type="range" min="100" max="1000" step="10" value="100" name="maxprice">
+                            <span class="pricevalue maxpricevalue">100 €</span>
+                    </div>
+                    <div style="width: 45%; padding: 0 15px; display: flex;">
+
+                        <label class="container-checkbox">Promotions
+                            <input type="checkbox" checked="checked" name="generatesaleprice">
+                            <span class="checkmark-box"></span>
+                        </label>
+                    </div>
+                </section>
 			  </div>
 
 			  <div class="Categories">
@@ -184,7 +193,7 @@ if(isset($_POST["generate"])){
                     </label>
 					<h2>Sous-catégories</h2>
                     <label class="container-checkbox">Générer des sous-catégories
-                        <input type="checkbox" name="generatesubcategories">
+                        <input type="checkbox" checked="checked" name="generatesubcategories">
                         <span class="checkmark-box"></span>
                     </label>
 				</section>
@@ -204,7 +213,7 @@ if(isset($_POST["generate"])){
 			  <div class="Qty">
 			  	<section>
 			  		<h2>Quantité de produits à générer</h2>
-			  		<input id="range-qty" type="range" min="10" max="100" step="10" value="10" name="productqty">
+			  		<input id="range-qty" type="range" min="10" max="150" step="10" value="10" name="productqty">
 					<span class="pricevalue productqtyvalue">10</span>
 			  	</section>
 			  </div>
@@ -218,6 +227,13 @@ if(isset($_POST["generate"])){
 			</div>
 
 		</form>
+
+        <div id="mentions">
+            <svg height="32" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true">
+                <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+            </svg>
+            <a href="https://github.com/sptaule/WooCsvGen">GitHub</a>
+        </div>
 
 	</div>
 
