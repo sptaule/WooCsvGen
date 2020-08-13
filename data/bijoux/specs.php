d2246b8777a61c0bbfdd6cd5def17cb296dd315e
sptaule <lecas83@gmail.com>
2020-08-13 20:31:42 +0200

    Added Images for the theme

--- data/bijoux/specs.php
+++ data/bijoux/specs.php
@@ -4,106 +4,181 @@ $listNames = array(
     array(
         "Name" => "Chevalière ovale",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "che01", "che02", "che03", "che04", "che05", "che06", "che07", "che08", "che09", "che10",
+            "che11", "che012", "che13", "che14", "che15", "che16", "che17", "che18", "che19", "che20",
+        )
     ),
     array(
         "Name" => "Chevalière carrée",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Argent"
+        "Subcategory" => "Argent",
+        "Images" => array(
+            "che01", "che02", "che03", "che04", "che05", "che06", "che07", "che08", "che09", "che10",
+            "che11", "che012", "che13", "che14", "che15", "che16", "che17", "che18", "che19", "che20",
+        )
     ),
     array(
         "Name" => "Chevalière rectangle",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Or"
+        "Subcategory" => "Or",
+        "Images" => array(
+            "che01", "che02", "che03", "che04", "che05", "che06", "che07", "che08", "che09", "che10",
+            "che11", "che012", "che13", "che14", "che15", "che16", "che17", "che18", "che19", "che20",
+        )
     ),
     array(
         "Name" => "Bague en rubis",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "che01", "che02", "che03", "che04", "che05", "che06", "che07", "che08", "che09", "che10",
+            "che11", "che012", "che13", "che14", "che15", "che16", "che17", "che18", "che19", "che20",
+        )
     ),
     array(
         "Name" => "Bague en ambre",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Argent"
+        "Subcategory" => "Argent",
+        "Images" => array(
+            "che01", "che02", "che03", "che04", "che05", "che06", "che07", "che08", "che09", "che10",
+            "che11", "che012", "che13", "che14", "che15", "che16", "che17", "che18", "che19", "che20",
+        )
     ),
     array(
         "Name" => "Bague en métal",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Or"
+        "Subcategory" => "Or",
+        "Images" => array(
+            "che01", "che02", "che03", "che04", "che05", "che06", "che07", "che08", "che09", "che10",
+            "che11", "che012", "che13", "che14", "che15", "che16", "che17", "che18", "che19", "che20",
+        )
     ),
     array(
         "Name" => "Bracelet en mailles",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "bra01", "bra02", "bra03", "bra04", "bra05", "bra06", "bra07", "bra08", "bra09", "bra10",
+            "bra11", "bra012", "bra13", "bra14", "bra15", "bra16", "bra17", "bra18", "bra19", "bra20",
+        )
     ),
     array(
         "Name" => "Bracelet croix",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Argent"
+        "Subcategory" => "Argent",
+        "Images" => array(
+            "bra01", "bra02", "bra03", "bra04", "bra05", "bra06", "bra07", "bra08", "bra09", "bra10",
+            "bra11", "bra012", "bra13", "bra14", "bra15", "bra16", "bra17", "bra18", "bra19", "bra20",
+        )
     ),
     array(
         "Name" => "Bracelet marin",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Or"
+        "Subcategory" => "Or",
+        "Images" => array(
+            "bra01", "bra02", "bra03", "bra04", "bra05", "bra06", "bra07", "bra08", "bra09", "bra10",
+            "bra11", "bra012", "bra13", "bra14", "bra15", "bra16", "bra17", "bra18", "bra19", "bra20",
+        )
     ),
     array(
         "Name" => "Collier",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "col01", "col02", "col03", "col04", "col05", "col06", "col07", "col08", "col09", "col10",
+            "col11", "col012", "col13", "col14", "col15", "col16", "col17", "col18", "col19", "col20",
+        )
     ),
     array(
         "Name" => "Chaîne",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "col01", "col02", "col03", "col04", "col05", "col06", "col07", "col08", "col09", "col10",
+            "col11", "col012", "col13", "col14", "col15", "col16", "col17", "col18", "col19", "col20",
+        )
     ),
     array(
         "Name" => "Pendentif",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "col01", "col02", "col03", "col04", "col05", "col06", "col07", "col08", "col09", "col10",
+            "col11", "col012", "col13", "col14", "col15", "col16", "col17", "col18", "col19", "col20",
+        )
     ),
     array(
         "Name" => "Boucles d'oreilles",
         "Category" => "Bijoux homme",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "bou01", "bou02", "bou03", "bou04", "bou05", "bou06", "bou07", "bou08", "bou09", "bou10",
+            "bou11", "bou12", "bou13", "bou14", "bou15", "bou16", "bou17", "bou18", "bou19", "bou20",
+        )
     ),
     array(
         "Name" => "Diadème",
         "Category" => "Bijoux femme",
-        "Subcategory" => "Acier"
-    ),
-    array(
-        "Name" => "Serre-tête",
-        "Category" => "Bijoux femme",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "dia01", "dia02", "dia03", "dia04", "dia05", "dia06", "dia07", "dia08", "dia09", "dia10",
+            "dia11", "dia012", "dia13", "dia14", "dia15", "dia16", "dia17", "dia18", "dia19", "dia20",
+        )
     ),
     array(
         "Name" => "Collier ras du cou",
         "Category" => "Bijoux femme",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "col01", "col02", "col03", "col04", "col05", "col06", "col07", "col08", "col09", "col10",
+            "col11", "col012", "col13", "col14", "col15", "col16", "col17", "col18", "col19", "col20",
+        )
     ),
     array(
         "Name" => "Montre",
         "Category" => "Accessoires",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "mon01", "mon02", "mon03", "mon04", "mon05", "mon06", "mon07", "mon08", "mon09", "mon10",
+            "mon11", "mon012", "mon13", "mon14", "mon15", "mon16", "mon17", "mon18", "mon19", "mon20",
+        )
     ),
     array(
         "Name" => "Piercing",
         "Category" => "Accessoires",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "pie01", "pie02", "pie03", "pie04", "pie05", "pie06", "pie07", "pie08", "pie09", "pie10",
+            "pie11", "pie012", "pie13", "pie14", "pie15", "pie16", "pie17", "pie18", "pie19", "pie20",
+        )
     ),
     array(
         "Name" => "Chaîne de cheville",
         "Category" => "Bijoux femme",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "col01", "col02", "col03", "col04", "col05", "col06", "col07", "col08", "col09", "col10",
+            "col11", "col012", "col13", "col14", "col15", "col16", "col17", "col18", "col19", "col20",
+        )
     ),
     array(
         "Name" => "Épingle à cheveux",
         "Category" => "Accessoires",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "epi01", "epi02", "epi03", "epi04", "epi05", "epi06", "epi07", "epi08", "epi09", "epi10",
+            "epi11", "epi012", "epi13", "epi14", "epi15", "epi16", "epi17", "epi18", "epi19", "epi20",
+        )
     ),
     array(
         "Name" => "Bouton de manchette",
         "Category" => "Accessoires",
-        "Subcategory" => "Acier"
+        "Subcategory" => "Acier",
+        "Images" => array(
+            "man01", "man02", "man03", "man04", "man05", "man06", "man07", "man08", "man09", "man10",
+            "man11", "man012", "man13", "man14", "man15", "man16", "man17", "man18", "man19", "man20",
+        )
     )
 );
