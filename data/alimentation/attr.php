f73b8a9dc0f9509e3d91d3bac0f16b6914342e40
sptaule <lecas83@gmail.com>
2020-08-13 20:31:20 +0200

    Added key for values

---
+++ data/alimentation/attr.php
@@ -0,0 +1,55 @@
+<?php
+
+$listAttr =
+    array(
+        array(
+            "AttrName" => "Packaging",
+            "AttrValues" => array(
+                "Carton",
+                "Plastique",
+                "Verre",
+                "Métal",
+                "Papier"
+            )
+        ),
+        array(
+            "AttrName" => "Score",
+            "AttrValues" => array(
+                "A+",
+                "A",
+                "B",
+                "C",
+                "D"
+            )
+        ),
+        array(
+            "AttrName" => "Allergènes",
+            "AttrValues" => array(
+                "Lait",
+                "Oeuf",
+                "Fruit à coque",
+                "Gluten",
+                "Soja"
+            )
+        ),
+        array(
+            "AttrName" => "Origine",
+            "AttrValues" => array(
+                "France",
+                "Espagne",
+                "Japon",
+                "Royaume-Uni",
+                "UE"
+            )
+        ),
+        array(
+            "AttrName" => "Label",
+            "AttrValues" => array(
+                "Bio",
+                "Ecocert",
+                "Vegan",
+                "Végétarien",
+                "Max Havelaar"
+            )
+        )
+    );
---
+++ data/bijoux/attr.php
@@ -0,0 +1,55 @@
+<?php
+
+$listAttr =
+    array(
+        array(
+            "AttrName" => "Couleur",
+            "AttrValues" => array(
+                "Argenté",
+                "Doré",
+                "Cristal",
+                "Blanc",
+                "Noir"
+            )
+        ),
+        array(
+            "AttrName" => "Taille",
+            "AttrValues" => array(
+                "XS",
+                "S",
+                "M",
+                "L",
+                "XL"
+            )
+        ),
+        array(
+            "AttrName" => "Matière",
+            "AttrValues" => array(
+                "Acier",
+                "Acier inoxydable",
+                "Argent",
+                "Or",
+                "Métal"
+            )
+        ),
+        array(
+            "AttrName" => "Provenance",
+            "AttrValues" => array(
+                "France",
+                "Espagne",
+                "Russie",
+                "Chine",
+                "UE"
+            )
+        ),
+        array(
+            "AttrName" => "Vertus",
+            "AttrValues" => array(
+                "Créativité",
+                "Dynamisme",
+                "Générosité",
+                "Protection",
+                "Sociabilité"
+            )
+        )
+    );
